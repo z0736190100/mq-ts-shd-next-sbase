38abcf514a6ea6a4c69f6122c07fa4007b988387
sburtsev <z0736190100@gmail.com>
2025-04-10 14:14:04 +0300

    starter project generated

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "mQuizz",
+  description: "The fastest way get shit done.",
 };
 
 const geistSans = Geist({
@@ -42,10 +42,10 @@ export default function RootLayout({
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Next.js Supabase Starter</Link>
-                    <div className="flex items-center gap-2">
+                    <Link href={"/"}>mQuizz</Link>
+                    {/*<div className="flex items-center gap-2">
                       <DeployButton />
-                    </div>
+                    </div>*/}
                   </div>
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                 </div>
@@ -57,13 +57,14 @@ export default function RootLayout({
               <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
                 <p>
                   Powered by{" "}
-                  <a
+                  {/*<a
                     href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
                     target="_blank"
                     className="font-bold hover:underline"
                     rel="noreferrer"
-                  >
-                    Supabase
+                  >*/}
+                  <a>
+                    good habit:
                   </a>
                 </p>
                 <ThemeSwitcher />
